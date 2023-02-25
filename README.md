5d556f9367c0d866a7cdddf97f7bff40e790b80c
RockyBndc <123609583+RockyBndc@users.noreply.github.com>
2023-02-25 20:52:18 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 2135-rapb-datepipe
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/2135-rapb-datepipe)
---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'pipes reactive';
+  presentDate = new Date();
+
+  todayO$ = new Observable<Date>((observer) => {
+    setInterval(() =>{
+      observer.next(new Date());
+    }, 1000);
+  })
+}
